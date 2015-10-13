f8a1686e27aa5e7e662c6e746d519bca5a85d148
Preksha Koirala <prekshakoirala@gmail.com>
2015-10-13 00:24:31 -0400

    some css stuffs

--- controller.js
+++ controller.js
@@ -1,6 +1,5 @@
 app.controller("MainController", function($scope){
 	$scope.mylist = [];
-	$scope.help = "Enter 'help'";
 	$scope.todo = "";
 	$scope.message = " ";
 	$scope.inputcolor = "black";
@@ -15,10 +14,10 @@ app.controller("MainController", function($scope){
 
 	$scope.showcommands = function(){
 			$scope.mylist = ['Enter "links" for my links',
-							 'Enter "Message" to text me',
+							 'Enter "message" to text me',
 							 'Enter "resume" to view my resume',
 							 'Enter "play" to play the game I made', 
-							 'Enter "song" to listen to my current favourite song',
+							 'Enter "song" to avoid boredom',
 							 'Enter "show" to see the full image',
 							 'Enter "emails" to list my emails',
 							 'Enter "chat" to chat with me'];
@@ -32,10 +31,10 @@ app.controller("MainController", function($scope){
 		else if ($scope.todo == "links"){
 			$scope.mylist=['www.facebook.com/preksha.koirala.7',
 							'www.github.com/PrekshaK',
-							 'https://www.linkedin.com/pub/preksha-koirala/99/7ab/38']
+							 'www.linkedin.com/in/prekshakoirala']
 
 		}
-		else if ($scope.todo == "Message"){
+		else if ($scope.todo == "message"){
 			$scope.ismessage = "yes";
 			$scope.mylist=["Type your MESSAGE in here"]
 
@@ -44,7 +43,7 @@ app.controller("MainController", function($scope){
 
 		}
 		else if($scope.todo == "show"){
-			$scope.mylist=['.', '.', '.', ' .', '. ', '.', '.', '.', ' .', '.', '.', '.', '. ', ' .', ',', '.', ' .', '.', '.', '.', '. ', ' .', ',', '.', ' .', '.', '.', '.', '. ', ' .', ','];
+			$scope.mylist=['.', '.', '.', ' .', '. ', '.', '.', '.', ' .', '.', '.', '.', '. ', ' .', '.', '.', ' .', '.', '.', '.', '. ', ' .', '.', '.', ' .', '.', '.', '.', '. ', ' .', '.'];
 		}
 		else if($scope.todo == "emails"){
 			$scope.mylist = ['prekshakoirala@gmail.com', 'preksha.koirala@bison.howard.edu'];
@@ -54,14 +53,14 @@ app.controller("MainController", function($scope){
 		}
 		else if($scope.todo == "chat"){
 			$scope.ischat = "yes";
-			$scope.mylist = ['Hey, how are you?   Thanks for checking my website. I have to go now. I have my class. TTYL'];
+			$scope.mylist = ['Hey, how are you?',   'Thanks for checking my website.',  ' I have to go now. I have my class. TTYL'];
 		}
 
 		else {
 			if($scope.ismessage == "yes"){
 					$scope.message = $scope.todo;
 					$scope.ismessage = "no";
-					$scope.mylist = ["Message recorded"];
+					$scope.mylist = ["message recorded"];
 
 			}else if ($scope.ischat == "yes"){
 				$scope.ischat = "no";
